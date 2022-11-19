3874d1dade63e41d28b4ccb9e4e6ef609c298a83
Shakib Rahman <cakoshakib@gmail.com>
2022-11-19 11:45:30 -0500

    verisk -> research

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -35,7 +35,7 @@ const About = () => {
         </li>
         <li>I am currently working on...</li>
         <ul>
-          <li>Working @ Verisk</li>
+          <li>TCS Research @ Aresty</li>
           <li>
             Building <a href="https://github.com/cakoshakib/ani-tui">AniTUI</a>
           </li>
