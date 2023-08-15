c8ac5c755901254cd2a12a1923e5c920b2bdf1d6
Shakib Rahman <cakoshakib@gmail.com>
2023-08-15 11:58:19 -0400

    minor refactor

--- src/App.tsx
+++ src/App.tsx
@@ -4,100 +4,107 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import "./App.css";
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import { Navbar, Nav } from "react-bootstrap";
 
 import Particles from "react-tsparticles";
 
+const BackgroundParticles = () => {
+  return (
+    <Particles
+      id="tsparticles"
+      options={{
+        fpsLimit: 60,
+        interactivity: {
+          detectsOn: "canvas",
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: "#404040",
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: "none",
+            enable: true,
+            outMode: "bounce",
+            random: false,
+            speed: 0.5,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              value_area: 800,
+            },
+            value: 15,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: "circle",
+          },
+          size: {
+            random: true,
+            value: 5,
+          },
+        },
+        detectRetina: true,
+      }}
+    />
+  );
+};
+
+const Navigation = () => {
+  return (
+    <Navbar
+      collapseOnSelect
+      className="m-auto justify-content-center"
+      expand="lg"
+      variant="dark"
+      id="navbar"
+    >
+      <Navbar.Brand href="/">Shakib Rahman</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/about" as="span">
+            <Link to="/about">About</Link>
+          </Nav.Link>
+          <Nav.Link href="/projects" as="span">
+            <Link to="/projects">Projects</Link>
+          </Nav.Link>
+          <Nav.Link href="/resume" as="span">
+            <a href="https://drive.google.com/file/d/162OgcVDWeylfIV-lOUOwhgC_IHzifI2C/view">
+              Resume
+            </a>
+          </Nav.Link>
+          <Nav.Link href="/contact" as="span">
+            <Link to="/contact">Contact</Link>
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
 const App = () => {
   return (
     <Router>
-      <Particles
-        id="tsparticles"
-        options={{
-          fpsLimit: 60,
-          interactivity: {
-            detectsOn: "canvas",
-            modes: {
-              bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#404040",
-            },
-            collisions: {
-              enable: true,
-            },
-            move: {
-              direction: "none",
-              enable: true,
-              outMode: "bounce",
-              random: false,
-              speed: 0.5,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                value_area: 800,
-              },
-              value: 15,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              random: true,
-              value: 5,
-            },
-          },
-          detectRetina: true,
-        }}
-      />
       <div className="container">
-        <Navbar
-          collapseOnSelect
-          className="m-auto justify-content-center"
-          expand="lg"
-          variant="dark"
-          id="navbar"
-        >
-          <Navbar.Brand href="/">Shakib Rahman</Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="/about" as="span">
-                <Link to="/about">About</Link>
-              </Nav.Link>
-              <Nav.Link href="/projects" as="span">
-                <Link to="/projects">Projects</Link>
-              </Nav.Link>
-              <Nav.Link href="/resume" as="span">
-                <a href="https://drive.google.com/file/d/162OgcVDWeylfIV-lOUOwhgC_IHzifI2C/view">
-                  Resume
-                </a>
-              </Nav.Link>
-              <Nav.Link href="/contact" as="span">
-                <Link to="/contact">Contact</Link>
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <BackgroundParticles />
+        <Navigation />
         <Switch>
           <Route path="/about">
             <About />
--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,19 +5,20 @@ import TypeWriterEffect from "react-typewriter-effect";
 const About = () => {
   return (
     <div id="aboutContainer">
-      <TypeWriterEffect
-        startDelay={50}
-        cursorColor="black"
-        multiText={["Hello, world."]}
-        multiTextDelay={300}
-        typeSpeed={200}
-      />
+      <div>
+        <TypeWriterEffect
+          cursorColor="black"
+          multiText={["Hello, world."]}
+          multiTextDelay={300}
+          typeSpeed={200}
+        />
+      </div>
       <h2>
         My name is <span id="name">Shakib</span>, here is a little about me!
       </h2>
       <ul>
         <li>
-          Currently a Junior at{" "}
+          Currently a Senior at{" "}
           <span id="college">Rutgers University New Brunswick</span>.
         </li>
         <li>
@@ -25,17 +26,14 @@ const About = () => {
           .
         </li>
         <li>
-          My career interests lie somewhere in the realm of{" "}
-          <b>Software Engineering</b>, <b>DevOps</b>, and{" "}
-          <b>Full-Stack Development</b>.
+          I am broadly interested in <b>distributed systems</b> and scaling <b>production infrastructure</b>{" "}
         </li>
         <li>
-          My favorite languages are <b>Java/TypeScript</b>, <b>Python</b>, and{" "}
+          My favorite languages are <b>TypeScript</b>, <b>Python</b>, and{" "}
           <b>Golang</b>.
         </li>
         <li>I am currently working on...</li>
         <ul>
-          <li>TCS Research @ Aresty</li>
           <li>
             Building <a href="https://github.com/ProjectWenda/wenda-backend">Wenda</a>
           </li>
@@ -43,7 +41,7 @@ const About = () => {
         </ul>
         <li>
           In my free time I enjoy chatting with friends, playing video games,
-          watching anime, and of course programming.
+          watching anime, and working on personal projects!
         </li>
       </ul>
     </div>
